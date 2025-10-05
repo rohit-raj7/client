14c1947d56d25e2a150a6e79da3f2c1b4260fe86
Rohit Raj <rohitraj70615@gmail.com>
2025-10-05 08:40:50 +0530

    x

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,8 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="bg-gray-900 text-white p-4 flex gap-6">
-      <Link to="/">Home</Link>
-      <Link to="/airdrops">Airdrops</Link>
+    <nav className="bg-gray-900 text-white p-4 flex gap-6 justify-center">
+      <Link to="/">Home</Link> 
       <Link to="/dashboard">Dashboard</Link>
       <Link to="/admin">Admin Panel</Link>
     </nav>
